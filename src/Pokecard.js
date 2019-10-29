13b766ee9ce48fb3e11615df3e385db4ab8179e0
Liviu Preda <liviupreda@gmail.com>
2019-10-29 10:20:48 +0200

    display card contents from pokemons array

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,23 +2,18 @@ import React, { Component } from 'react';
 import './Pokecard.css';
 
 class Pokecard extends Component {
-  static defaultProps = {
-    id: 4,
-    name: 'Charmander',
-    type: 'fire',
-    experience: 62
-  };
   render() {
-    const p = this.props;
+    const { name, type, exp, imgSrc } = this.props;
     return (
       <div className='Pokecard'>
-        <p className='Pokecard-title'>{p.name}</p>
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-          alt='poke'
-        />
-        <p>Type: {p.type}</p>
-        <p>EXP: {p.experience}</p>
+        <div>
+          <h1 className='Pokecard-title'>{name}</h1>
+        </div>
+        <img src={imgSrc} alt='poke' />
+        <div>
+          <p>Type: {type}</p>
+          <p>EXP: {exp}</p>
+        </div>
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
 import './Pokedex.css';
+import { pokemons } from './pokemons';
 
 class Pokedex extends Component {
   render() {
+    const index = 7;
     return (
       <div className='Pokedex'>
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
-        <Pokecard />
+        <Pokecard
+          id={pokemons[index].id}
+          name={pokemons[index].name}
+          type={pokemons[index].type}
+          exp={pokemons[index].base_experience}
+          imgSrc={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemons[index].id}.png`}
+        />
       </div>
     );
   }
