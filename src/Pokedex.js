37f466d1dc259220ea77aca6792f17da591fd863
Liviu Preda <liviupreda@gmail.com>
2019-10-29 11:24:42 +0200

    display hi-res images

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,19 +3,29 @@ import Pokecard from './Pokecard';
 import { pokemons } from './pokemons';
 import './Pokedex.css';
 
+function padId(id) {
+  if (id < 10) return '00' + id;
+  else if (id < 100) return '0' + id;
+  else return id;
+}
+
 class Pokedex extends Component {
   render() {
     return (
       <div className='Pokedex'>
         {pokemons.map(el => {
           return (
-            <Pokecard
-              id={el.id}
-              name={el.name}
-              type={el.type}
-              exp={el.base_experience}
-              imgSrc={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${el.id}.png`}
-            />
+            <>
+              <Pokecard
+                id={el.id}
+                name={el.name}
+                type={el.type}
+                exp={el.base_experience}
+                imgSrc={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${padId(
+                  el.id
+                )}.png`}
+              />
+            </>
           );
         })}
       </div>
