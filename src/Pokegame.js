0a30895caef1ab234784062d17b4fbb27abae972
Liviu Preda <liviupreda@gmail.com>
2019-10-29 14:49:13 +0200

    move array shuffle and slice logic to Pokegame comp; add array prop to Pokedex comp

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import Pokedex from './Pokedex';
+import { pokemons } from './pokemons';
 import './Pokegame.css';
+
+function shuffleArray(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 class Pokegame extends Component {
   render() {
+    shuffleArray(pokemons);
     return (
       <div>
-        <Pokedex />
-        <Pokedex />
+        <Pokedex array={pokemons.slice(0, 4)} />
+        <Pokedex array={pokemons.slice(4, 8)} />
       </div>
     );
   }
--- src/test_Pokedex.js
+++ src/test_Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
+import { pokemons } from './pokemons';
 import './Pokedex.css';
 
 function padId(id) {
@@ -8,13 +9,34 @@ function padId(id) {
   else return id;
 }
 
+function shuffleArray(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 class Pokedex extends Component {
   render() {
+    shuffleArray(pokemons);
     let expCounter = 0;
-    const { array } = this.props;
+    const { totalExp } = this.props;
     return (
       <div className='Pokedex'>
-        {array.map((el, i) => {
+        {pokemons.slice(0, 4).map((el, i) => {
           expCounter += el.base_experience;
           return (
             <div key={i}>
